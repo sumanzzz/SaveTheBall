a1779196c897fa5f0df082d66367d46e6d4985dc
Reegan <reegansuman@gmail.com>
2025-08-24 14:16:18 +0530

    Enemy Core logic

---
+++ Enemy.lua
@@ -0,0 +1,33 @@
+local love = require("love")
+
+function Enemy()
+    return{
+        level = 1,
+        radius = 20,
+        x = -10,
+        y = -50,
+
+        move = function(self , player_x , player_y)
+            if player_x - self.x > 0 then
+                self.x = self.x + self.level
+            elseif player_x - self.x < 0 then
+                self.x = self.x - self.level
+            end
+            if player_y - self.y > 0 then
+                self.y = self.y + self.level
+            elseif player_y - self.y < 0 then
+                self.y = self.y - self.level
+            end
+           
+        end,
+        
+        draw = function(self)
+            love.graphics.setColor(1,0,0.5)
+
+            love.graphics.circle("fill",self.x,self.y, self.radius)
+            
+            love.graphics.setColor(1,1,1)
+        end
+    }
+end
+return Enemy
--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 local love = require("love")
+local enemy = require("Enemy")
 
 local game = {
+    difficulty = 1,
     state = {
-        menu = true,
+        menu = false,
         paused = false,
-        running = false,
+        running = true,
         ended = false
     }
 
@@ -14,21 +16,32 @@ local player = {
     x = 30,
     y = 30
 }
+local enemies = {}
+
 function love.load()
     love.mouse.setVisible(false)
+    table.insert(enemies, 1, enemy())
 end
 
 function love.update()
     player.x, player.y = love.mouse.getPosition()
+
+    for i = 1, #enemies do
+        enemies[i]:move(player.x, player.y)
+    end
 end
 
 function love.draw()
-    love.graphics.printf("FPS:"..love.timer.getFPS(),love.graphics.newFont(16),10,10,love.graphics.getWidth())
+    love.graphics.printf("FPS:" .. love.timer.getFPS(), love.graphics.newFont(16), 10, love.graphics.getHeight() - 30,
+        love.graphics.getWidth())
     if game.state["running"] then
-        love.graphics.circle("fill", player.x, player.y, player.radius )
+        for i = 1, #enemies do
+            enemies[i]:draw()
+        end
+        love.graphics.circle("fill", player.x, player.y, player.radius)
     end
     if not game.state["running"] then
-        love.graphics.circle("fill", player.x, player.y, player.radius/2 )
+        love.graphics.circle("fill", player.x, player.y, player.radius / 2)
     end
 
 end
