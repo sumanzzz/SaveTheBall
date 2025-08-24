298c995c8a00c4edd9e0c8261aea234057ce5155
Reegan <reegansuman@gmail.com>
2025-08-24 13:18:04 +0530

    Added player,game states,set cursor visibility to false and fps meter

--- main.lua
+++ main.lua
@@ -1,14 +1,34 @@
 local love = require("love")
 
-function love.load()
+local game = {
+    state = {
+        menu = true,
+        paused = false,
+        running = false,
+        ended = false
+    }
 
+}
+local player = {
+    radius = 30,
+    x = 30,
+    y = 30
+}
+function love.load()
+    love.mouse.setVisible(false)
 end
 
-
 function love.update()
-
+    player.x, player.y = love.mouse.getPosition()
 end
 
 function love.draw()
-    
+    love.graphics.printf("FPS:"..love.timer.getFPS(),love.graphics.newFont(16),10,10,love.graphics.getWidth())
+    if game.state["running"] then
+        love.graphics.circle("fill", player.x, player.y, player.radius )
+    end
+    if not game.state["running"] then
+        love.graphics.circle("fill", player.x, player.y, player.radius/2 )
+    end
+
 end
