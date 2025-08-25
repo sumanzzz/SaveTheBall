55a604139f520c82e5c239c753b21c6ba24a0ed5
Reegan <reegansuman@gmail.com>
2025-08-25 20:59:17 +0530

    High score implemented

--- main.lua
+++ main.lua
@@ -12,8 +12,9 @@ local game = {
         running = false,
         ended = false
     },
+    highScore = 0,
     points = 0,
-    levels = {15, 30, 60, 120}
+    levels = {15,25, 30,50, 60, 120}
 
 }
 local fonts = {
@@ -88,19 +89,24 @@ function love.update(dt)
     player.x, player.y = love.mouse.getPosition()
 
     if game.state["running"] then
+        
         for i = 1, #enemies do
             if not enemies[i]:checkTouched(player.x, player.y, player.radius) then
                 enemies[i]:move(player.x, player.y)
 
                 for i = 1, #game.levels do
                     if math.floor(game.points) == game.levels[i] then
-                        table.insert(enemies, 1, enemy(game.difficulty * (i + 1)))
+                        table.insert(enemies, 1, enemy(game.difficulty * (i + 2)))
 
                         game.points = game.points + 1
                     end
                 end
             else
                 ChangeGameState("ended")
+                if game.points > game.highScore then
+                    game.highScore = game.points
+        
+                end
             end
 
         end
@@ -113,8 +119,11 @@ function love.draw()
     love.graphics.setFont(fonts.medium.font)
     love.graphics.printf("FPS:" .. love.timer.getFPS(), fonts.medium.font, 10, love.graphics.getHeight() - 30,
         love.graphics.getWidth())
+        
     if game.state["running"] then
         love.graphics.printf(math.floor(game.points), fonts.large.font, 0, 10, love.graphics.getWidth(), "center")
+        love.graphics.printf("High Score : " .. math.floor(game.highScore), fonts.medium.font, 10, 10,love.graphics.getWidth(),"left")
+        
         for i = 1, #enemies do
             enemies[i]:draw()
         end
